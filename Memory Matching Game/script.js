8fc536d1b94e76aa81bed603fb0f6c6aa8a6c864
jung-chaewon <131144717+jung-chaewon@users.noreply.github.com>
2024-06-13 18:29:44 +0900

    Add files via upload

---
+++ Memory Matching Game/script.js
@@ -0,0 +1,67 @@
+const container = document.getElementById('container');
+const cardsLength = 12;
+const cards = [];
+let previousShownCard;
+let icons = [
+    'linkedin',
+    'instagram',
+    'twitter',
+    'whatsapp',
+    'youtube',
+    'facebook'
+];
+
+// Copy the icons again so we can use them in the cards
+icons.push(...icons);
+
+// Shuffle the icons
+for (let i = 0; i < 100; i++) {
+    const idx1 = Math.floor(Math.random() * icons.length);
+    const idx2 = Math.floor(Math.random() * icons.length);
+    const temp = icons[idx1];
+    icons[idx1] = icons[idx2];
+    icons[idx2] = temp;
+}
+
+for (let i = 0; i < cardsLength; i++) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `<div class="front">
+                        <i class="fab fa-${icons[i]}"></i>
+                      </div>
+                      <div class="back"><small>Click me</small></div>`;
+
+    card.addEventListener('click', () => {
+        if (!card.classList.contains('show')) {
+            card.classList.add('show');
+
+            if (!previousShownCard) {
+                previousShownCard = card;
+            } else {
+                const iconOne = previousShownCard.querySelector('i').classList[1];
+                const iconTwo = card.querySelector('i').classList[1];
+
+                if (iconOne !== iconTwo) {
+                    const temp = previousShownCard;
+                    setTimeout(() => {
+                        temp.classList.remove('show');
+                        card.classList.remove('show');
+                    }, 1000);
+                }
+                previousShownCard = undefined;
+            }
+
+            // Check if all cards are shown
+            if (document.querySelectorAll('.card.show').length === cardsLength) {
+                setTimeout(() => {
+                    alert('Congratulations! You have finished the game!');
+                    return window.location.reload();
+                }, 500); // Add a slight delay to ensure the last card is shown before the alert
+            }
+        }
+    });
+
+    cards.push(card);
+    container.appendChild(card);
+    console.log("Cards", card);
+}
